8009a0ebfaa2049d828ba73c65adfbd823bf881d
Arundhati Gadge <51483255+Arundhati1212@users.noreply.github.com>
2024-05-27 02:00:59 -0700

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,35 @@
+Eventify
+As a frontend developer dedicated to crafting immersive web experiences, I'm excited to unveil Eventify, my latest project. With Eventify, I've leveraged the power of React, TanStack Query, and cutting-edge React functionalities to deliver a dynamic and efficient event management platform.
+
+Project Overview
+Eventify: Eventify is a dynamic event management platform that allows users to effortlessly search, view, and manage events. Built with React and TanStack Query, this application ensures optimal performance and a seamless user experience by streamlining data fetching and caching.
+
+Key Features and Technologies Utilized
+TanStack Query: Leveraged TanStack Query to streamline data fetching and caching, ensuring optimal performance and scalability while reducing boilerplate code and complexity.
+Dynamic Event Search: Implemented a dynamic search functionality allowing users to search for events based on keywords, enhancing user experience and accessibility.
+React Routing: Utilized React Router for client-side routing, enabling smooth navigation between different pages and ensuring a seamless browsing experience for users.
+
+
+How to Install and Run the Project
+To run MealMate locally, follow these steps:
+1.	Clone the repository: git clone https://github.com/Arundhati1212/Eventify.git
+2.	Navigate to the project directory: cd mealmate 
+3.	Install dependencies: npm install 
+4.	Start the development server: 
+    /backend : npm start
+    / : npm run dev 
+The application should now be running on http://localhost:5173.
+
+How to Use the Project
+1.	Search for Events:
+•	Use the search bar to find events based on keywords.
+2.	Navigate Through Pages:
+•	Utilize the navigation links to switch between different pages seamlessly.
+3.	View Event Details:
+•	Click on events to view detailed information.
+
+Contributors
+•	Arundhati Gadge - linkedin.com/in/arundhati-gadge/
+
+
+
